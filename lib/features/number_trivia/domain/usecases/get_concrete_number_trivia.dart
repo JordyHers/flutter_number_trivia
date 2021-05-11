fe3cd0dfbf0c847ef03877ccb9c2c430437dedf8
jordyhers <jordy925@gmail.com>
2021-05-11 21:26:28 +0300

    2.Commmit test widget

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,7 +1,16 @@
+import 'package:flutter_number_trivia/core/error/failures.dart';
+import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:meta/meta.dart';
+import 'package:dartz/dartz.dart';
+
 
 class GetConcreteNumberTrivia{
   final NumberTriviaRepository repository;
-
   GetConcreteNumberTrivia(this.repository);
+
+Future<Either<Failure, NumberTrivia>> execute ({@required int number}) async {
+  return await repository.getConcreteNumberTrivia(number);
+
+}
 }
