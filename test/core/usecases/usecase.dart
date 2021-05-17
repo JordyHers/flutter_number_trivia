8ddeb3444b577e3baf48b8060f3e946a7a3af88c
jordyhers <jordy925@gmail.com>
2021-05-17 16:52:59 +0300

    2.Commmit test widget

--- test/core/usecases/usecase.dart
+++ test/core/usecases/usecase.dart
@@ -1,4 +1,6 @@
-
+import 'package:dartz/dartz.dart';
+import 'package:flutter_number_trivia/core/error/failures.dart';
+import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 abstract class UseCase <Type, Params>{
   Future<Either<Failure,NumberTrivia>> call();
